150a3f182330d5a4bb1a4788512beef78f59b928
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-03-13 14:28:42 +0100

    Rewrite config parser to use pyserde and dataclasses
    Using well-defined dataclasses allows for proper type hinting

--- TORS/manager/manager.py
+++ TORS/manager/manager.py
@@ -1,150 +1,184 @@
+from typing import Callable
 from pyTORS import ScenarioFailedError
 from manager.simulator import Simulator
+from planner.planner import Planner
 import importlib
-from time import time
+from time import time, perf_counter
 from contextlib import contextmanager
+import logging
+from typing import Optional
+from typing_extensions import Self
+import functools
+from manager.config import AgentConfig, EpisodeConfig
+
+
+def time_in_planner(func):
+    @functools.wraps(func)
+    def wrapper_time_in_planner(self: Self, *args, **kwargs):
+        start = perf_counter()
+        ret = func(self, *args, **kwargs)
+        self.sol_runtime += start - perf_counter()
+        return ret
+
+    return wrapper_time_in_planner
+
 
 class Manager:
-    
-    def __init__(self, episode_config, agent_config):
+    def __init__(self, episode_config: EpisodeConfig, agent_config: AgentConfig):
+        self.logger = logging.getLogger(
+            f"{self.__class__.__module__}.{self.__class__.__name__}"
+        )
         self.episode_config = episode_config
         self.agent_config = agent_config
         self.simulator = Simulator(episode_config)
+        self.sol_runtime = 0
         self.planner = self.get_planner()
         self.print_episode_info()
         self.print_agent_info()
-    
+
     def print_episode_info(self):
-        self.print("M> ### Episode info ###")
-        self.print("M> Data folder: {}".format(self.episode_config['data folder']))
-        self.print("M> Scenario(s): {}".format(self.episode_config['scenario']))
-        self.print("M> Number of runs: {}".format(self.episode_config['n_runs']))
-        self.print("M> Maximum number of trains: {}".format(self.episode_config['max_trains']))
-    	
+        self.logger.info("### Episode info ###")
+        self.logger.info("Data folder: {}".format(self.episode_config.data_folder))
+        self.logger.info("Scenario(s): {}".format(self.episode_config.scenario))
+        self.logger.info("Number of runs: {}".format(self.episode_config.n_runs))
+        self.logger.info(
+            "Maximum number of trains: {}".format(self.episode_config.max_trains)
+        )
+
     def print_agent_info(self):
         self.print("M> ### Agent info ###")
-        planner_class = self.agent_config['class']
+        planner_class = self.agent_config.agent_class
         self.print("M> Agent class: {}".format(planner_class))
-        if planner_class in self.agent_config:
-            config = self.agent_config[planner_class]
-            for key, val in config.items():
-                self.print("M> \t{}: {}".format(key, val))
-      
-    def run(self):
-        self.sol_runtime = 0
-        self.sim_runtime = 0
-        self.gen_runtime = 0
-        
-        sim_start = time()
-        self.simulator.start()
-        self.sim_runtime += time() - sim_start
+        # if planner_class in self.agent_config:
+        #     config = self.agent_config[planner_class]
+        #     for key, val in config.items():
+        #         self.print("M> \t{}: {}".format(key, val))
 
-        start = time()
+    @time_in_planner
+    def initialize_planner(self):
+        self.logger.debug("Initializing planner")
+        engine, location = self.simulator.get_engine(), self.simulator.get_location()
         with timeout(self, 10):
-            self.planner.initialize(self.simulator.get_engine(), self.simulator.get_location())
-        self.sol_runtime += time() - start
+            self.planner.initialize(engine, location)
 
-        results = {}
-        fails = {}
-        for t in range(1, self.simulator.get_max_trains()+1):
-            results[t] = 0
-            fails[t] = 0
-            
-            gen_start = time()
-            self.simulator.set_n_trains(t)
-            self.gen_runtime += time() - gen_start
-            
-            self.print("M> ###############################")
-            self.print("M> ### Testing with {:3} trains ###".format(t))
-            self.print("M> ###############################")
-            for r in range(self.episode_config.n_runs):
-                if self.get_remaining_planning_time() < 0 and self.get_remaining_planning_time() != -1: break
-                self.print("M> ### Run {:3}                 ###".format(r+1))
-                try:
-                    result, failure = self.run_one()
-                except TimeoutError:
-                    break
-                results[t] += result
-                fails[t] += failure
-                if failure: self.print("M> Scenario failed")
-                else: self.print("M> Result: {}".format(result))
-            self.print("M> Average score: {}%, Failures: {}%".format(results[t]/self.episode_config.n_runs * 100., fails[t]/self.episode_config.n_runs * 100))
-            if fails[t] > 0 or results[t] == 0: break
-            if self.get_remaining_planning_time() < 0 and self.get_remaining_planning_time() != -1: break
-        with timeout(self, 10):
-            self.planner.close()
-        for t in results:
-            self.print("M> {} Trains |\tAverage score: {}%, Failures: {}%".format(t, results[t]/self.episode_config.n_runs * 100., fails[t]/self.episode_config.n_runs * 100))
-        print("M> Time spent in solver: {}s".format(self.sol_runtime))
-        print("M> Time spent in simulator: {}s".format(self.sim_runtime))
-        print("M> Time spent in scenario generator: {}s".format(self.gen_runtime))
-        print("M> Total accounted time: {}s".format(self.sol_runtime + self.sim_runtime + self.gen_runtime))    
-    
-    def run_one(self):
-        failure = 0
-        self.simulator.reset()
-        start = time()
+    @time_in_planner
+    def reset_planner(self):
+        self.logger.debug("Resetting planner")
         with timeout(self, 10):
             self.planner.reset()
-        self.sol_runtime += time() - start
-        while True:
-            sim_start = time()
-            state = self.simulator.get_state()
-            self.sim_runtime += time() - sim_start
-            if not self.simulator.is_active(): break
-            try:
-                start = time()
-                with timeout(self):
-                    action = self.planner.get_action(state)
-                self.sol_runtime += time() - start
-                sim_start = time()
-                if action is None:
-                    valid_actions = self.simulator.get_engine().get_valid_actions(state)
-                    if len(valid_actions) == 0: break
-                    raise ScenarioFailedError("The planner returned no action, but valid actions are still available.")
-                elif not self.simulator.apply_action(action): break
-                self.sim_runtime += time() - sim_start
-            except TimeoutError:
-                self.sol_runtime += time() - start
-                self.print("M> Timeout reached.")
-                break
-            except Exception as e:
-                print(e)
-                raise e
-                failure = 1
-                break
-        result = self.simulator.get_result()
-        start = time()
+
+    @time_in_planner
+    def get_planner_action(self, state):
+        self.logger.debug("Getting state from planner")
+        with timeout(self):
+            return self.planner.get_action(state)
+
+    @time_in_planner
+    def report_planner_result(self, result):
+        self.logger.debug("Reporting result back to planner")
         with timeout(self, 10):
             self.planner.report_result(result)
-        self.sol_runtime += time() - start
+
+    def lack_of_action_not_valid(self, state):
+        valid_actions = self.simulator.get_engine().get_valid_actions(state)
+        if len(valid_actions) != 0:
+            return True
+        return False
+
+    def apply_action(self, state, action):
+        if action is None:
+            if self.lack_of_action_not_valid(state):
+                raise ScenarioFailedError(
+                    "The planner returned no action, but valid actions are still available."
+                )
+            else:
+                return False
+        elif action is not None:
+            self.simulator.apply_action(action)
+            return True
+
+    def run(self, n_trains=2):
+        failure = False
+
+        self.simulator.start()
+
+        self.initialize_planner()
+
+        self.simulator.generate_scenario(n_trains)
+
+        planning_time_left = self.get_remaining_planning_time()
+        if planning_time_left < 0 and planning_time_left != -1:
+            return self.simulator.get_result(), True
+
+        # Close running engine, generate new scenario
+        self.simulator.reset()
+
+        self.reset_planner()
+
+        # Main simulation loop
+        while True:
+            state = self.simulator.get_state()
+            if not self.simulator.is_active():  # If scenario not failed or ended
+                break
+
+            # Step simulation and handle possible (expected exceptions)
+            try:
+                action = self.get_planner_action(state)
+                should_continue = self.apply_action(state, action)
+
+                if not should_continue:
+                    break
+
+            except TimeoutError:
+                self.logger.info("Timeout reached.")
+                failure = True
+                break
+            except ScenarioFailedError as e:
+                self.logger.info(e)
+                failure = True
+                break
+
+        result = self.simulator.get_result()
+        self.report_planner_result(result)
+
         return result, failure
-     
+
     def print(self, m):
-        if self.episode_config.verbose >= 1: print(m)   
-                
-    def get_planner(self):
-        planner_str = self.agent_config['class']
-        planner_lst = planner_str.split('.')
+        if self.episode_config.verbose >= 1:
+            print(m)
+
+    def get_planner(self) -> Planner:
+        planner_str = self.agent_config.agent_class
+        planner_lst = planner_str.split(".")
         _module = importlib.import_module(".".join(planner_lst[:-1]))
         _class = getattr(_module, planner_lst[-1])
-        if planner_str in self.agent_config:
-            config = self.agent_config[planner_str]
-        else: config = {} 
-        self.agent_config["time_limit"] = self.episode_config.time_limit
+        # if planner_str in self.agent_config:
+        # config = self.agent_config[planner_str]
+        # else:
+        config = AgentConfig()
+        # self.agent_config["time_limit"] = self.episode_config.time_limit
         planner = _class(self.agent_config, config)
         return planner
 
     # Get the remaining time left for the planner (in seconds)
-    def get_remaining_planning_time(self):
+    def get_remaining_planning_time(self) -> float:
         time_limit = self.episode_config.time_limit
-        if time_limit == -1: return -1
+        if time_limit == -1:
+            return -1
         return float(time_limit) - self.sol_runtime
 
+
+def time_function(f: Callable):
+    """Execute a function and return the amount of time it took."""
+    start = time()
+    f()
+    return time() - start
+
+
 # Code for time out #######################################################################
-#From https://www.jujens.eu/posts/en/2018/Jun/02/python-timeout-function/
-#From https://stackoverflow.com/questions/492519/timeout-on-a-function-call/494273#494273
-#Note, only works on UNIX
+# From https://www.jujens.eu/posts/en/2018/Jun/02/python-timeout-function/
+# From https://stackoverflow.com/questions/492519/timeout-on-a-function-call/494273#494273
+# Note, only works on UNIX
 try:
     import signal
 
@@ -162,7 +196,7 @@ try:
             # Register a function to raise a TimeoutError on the signal.
             signal.signal(signal.SIGALRM, raise_timeout)
             # Schedule the signal to be sent after ``time``.
-            signal.alarm(int(time+2)) # add some extra time 
+            signal.alarm(int(time + 2))  # add some extra time
         timeout_error = False
         try:
             yield
@@ -176,12 +210,13 @@ try:
             if timeout_error:
                 raise TimeoutError()
 
-except: # If the package signal cannot be imported
-    
+except:  # If the package signal cannot be imported
+
     @contextmanager
     def timeout(manager, minimum=None):
         if manager.get_remaining_planning_time() != -1:
             raise NotImplementedError("Time out is only supported on UNIX systems")
         yield
+
 
 # End code for time out ###################################################################
