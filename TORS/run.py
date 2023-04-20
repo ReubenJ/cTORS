394ff17c57bde872ec8ae69c5ee8107a843db33d
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-04-20 07:28:19 +0200

    Add seed and epsilon args to run script

--- TORS/run.py
+++ TORS/run.py
@@ -1,15 +1,18 @@
 from manager.manager import Manager
-from manager.config import EpisodeConfig, AgentConfig
+from manager.config import EpisodeConfig, AgentConfig, GreedyAgentConfig
 import time
 import argparse
 from serde.json import from_json
+import time
 
 
-def initialize_manager(episode, agent, verbose=0):
+def initialize_manager(episode, agent, seed, epsilon, verbose=0):
     with open(episode, "r") as episode_config_file:
         episode_config_serde = from_json(EpisodeConfig, episode_config_file.read())
     with open(agent, "r") as agent_config_file:
-        agent_config_serde = from_json(AgentConfig, agent_config_file.read())
+        agent_config_serde = from_json(GreedyAgentConfig, agent_config_file.read())
+    agent_config_serde.seed = seed
+    agent_config_serde.epsilon = epsilon
 
     manager = Manager(episode_config_serde, agent_config_serde)
 
@@ -53,9 +56,22 @@ if __name__ == "__main__":
         default=1,
         type=int,
     )
+    parser.add_argument(
+        "-s",
+        "--seed",
+        help="Seed for the random number generator",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "--epsilon",
+        help="Epsilon value for the epsilon-greedy policy",
+        default=0.5,
+        type=float,
+    )
     args = parser.parse_args()
     start = time.time()
-    manager = initialize_manager(args.episode, args.agent)
+    manager = initialize_manager(args.episode, args.agent, args.seed, args.epsilon)
 
     if args.train:
         raise NotImplementedError("Not yet implemented")
@@ -64,6 +80,7 @@ if __name__ == "__main__":
             n_trains=args.number_trains, result_save_path=args.result_path
         )
         print(f"Scenario failed: {failed}")
+    # print(f"Success rate: {0/100}")
     print("Total running time: {}".format(time.time() - start))
 
     # print("\n***** Summary Metrics *****")
